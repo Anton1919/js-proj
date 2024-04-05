1874a26ae789e0317185d23a8ff1cd5c7da00bc7
Anton1919 <bf3kenzo@mail.ru>
2024-04-05 12:05:52 +0300

    implemented methods of classes for render html text

---
+++ src/components/excel/Excel.js
@@ -0,0 +1,21 @@
+export class Excel {
+  constructor(selector, option) {
+    this.$el = document.querySelector(selector);
+    this.components = option.components || [];
+  }
+
+  getRoot() {
+    const $root = document.createElement('div');
+
+    this.components.forEach((Component) => {
+      const component = new Component;
+      $root.insertAdjacentHTML('beforeend', component.toHTML());
+    });
+
+    return $root;
+  }
+
+  render() {
+    this.$el.append(this.getRoot());
+  }
+}
--- webpack.config.js
+++ webpack.config.js
@@ -2,43 +2,83 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
-const buildResolvers = require('./config/webpack/buildResolvers');
-const buildLoaders = require('./config/webpack/buildLoaders');
-const buildPlugins = require('./config/webpack/buildPlugins');
-const buildDevServer = require('./config/webpack/buildDevServer');
-const jsLoaders = require('./config/loaders/buildBabelLoader');
 
 const IS_DEV = process.env.NODE_ENV === 'development';
 const IS_PROD = !IS_DEV;
+
+function jsLoaders() {
+  const loaders = [
+    {
+      loader: 'babel-loader',
+      options: {
+        presets: ['@babel/preset-env'],
+      },
+    },
+  ];
+
+  if (IS_DEV) {
+    loaders.push('eslint-loader');
+  }
+
+  return loaders;
+}
 
 const fileName = (extension) => {
   return IS_DEV ? `bundle.${extension}` : `bundle.[contenthash].${extension}`;
 };
 
-const options = {
-  MiniCssExtractPlugin,
-  HtmlWebpackPlugin,
-  CopyPlugin,
-  fileName,
-  jsLoaders,
-  path,
-  IS_PROD,
-  IS_DEV,
-};
-
 module.exports = {
-  entry: './src/index.js',
+  entry: path.resolve(__dirname, 'src', 'index.js'),
   mode: 'development',
   output: {
     filename: fileName('js'),
     path: path.resolve(__dirname, 'build'),
     clean: true,
   },
-  resolve: buildResolvers(options),
-  plugins: buildPlugins( options),
+  resolve: {
+    extensions: ['.js'],
+    preferAbsolute: true,
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '@core': path.resolve(__dirname, 'src', 'core'),
+    },
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: fileName('css'),
+    }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src', 'index.html'),
+      minify: {removeComments: IS_PROD, collapseWhitespace: IS_PROD},
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'src', 'favicon.ico'),
+          to: path.resolve(__dirname, 'build'),
+        },
+      ],
+    }),
+  ],
   module: {
-    rules: buildLoaders(options),
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: jsLoaders(),
+      },
+    ],
   },
   devtool: IS_DEV ? 'inline-source-map' : false,
-  devServer: buildDevServer(options),
+  devServer: {
+    port: 3000,
+    open: IS_DEV,
+    hot: IS_DEV,
+    watchFiles: [path.resolve(__dirname, './src/*.html')],
+  },
 };
+
